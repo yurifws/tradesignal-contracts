8be82b1856f12a95747fa86fdd9b4a3b4bbe903f
yurifws <yurifws@gmail.com>
2026-01-21 23:45:43 -0300

    <feat> initial setup hardhat

---
+++ contracts/SignalMarket.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.8.20;
+
+contract SignalMarket {
+}
---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
