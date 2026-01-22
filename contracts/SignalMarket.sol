fc3b0166e435e0e936d5948f71e4c54b4654cf4f
yurifws <yurifws@gmail.com>
2026-01-22 00:11:33 -0300

    <feat> basic variables for signal

--- contracts/SignalMarket.sol
+++ contracts/SignalMarket.sol
@@ -1,4 +1,32 @@
 pragma solidity ^0.8.20;
 
 contract SignalMarket {
+
+    enum Direction { 
+        Bullish, 
+        Bearish
+    }
+
+    enum SignalStatus { 
+        Active, 
+        Resolved, 
+        Expired,
+        Cancelled
+    }
+
+    struct Signal {
+        address trader;
+        Direction direction;
+        SignalStatus status;
+        bool isCorrect;
+        uint256 id;
+        uint256 targetPrice;
+        uint256 deadline;
+        uint256 fee;
+        string asset;
+    }
+
+    mapping(uint256 => Signal) public signals;
+    uint256 public nextSignalId;
+
 }
