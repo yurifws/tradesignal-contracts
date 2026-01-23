ece96dd17c892e67e7b7ed03630bd6be256d9d52
yurifws <yurifws@gmail.com>
2026-01-23 00:16:13 -0300

    <feat> update dependences to deploy success

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    hardhat: {},
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
